b520e2743336691d6f1321987b1dac6af8dab11f
Rose <rose@lain.org.uk>
2019-12-03 20:43:02 +0000

    works

--- 2a.hs
+++ 2a.hs
@@ -37,8 +37,8 @@ listToOpcode xs                    = Nothing
 
 -- | Given a vector of integers and an address
 -- return the Opcode if a valid one exists at that point.
-atOffset :: Addr -> RAM -> Maybe Opcode
-atOffset ix xs =
+atOffset :: RAM -> Addr -> Maybe Opcode
+atOffset xs ix =
   case V.take 4 $ V.drop ix xs of
     []  -> Nothing
     xs' -> listToOpcode xs'
@@ -52,18 +52,24 @@ perform (Add x y dest) ram = ram V.// [(dest, x' + y')]
   where (x', y') = (ram !? x, ram !? y)
 perform (Mul x y dest) ram = ram V.// [(dest, x' * y')]
   where (x', y') = (ram !? x, ram !? y)
-perform Halt ram = ram
+perform Halt ram = error ("HALTED AT STATE: " ++ show ram)
 
 -- | Construct a vector of integers from a list of bytestrings.
 -- It's optimistic - if the parse fails for an element, the value is omitted.
 parseProgram :: [ByteString] -> RAM
 parseProgram = V.fromList . catMaybes . map (fmap fst . BS.readInt)
 
+run :: RAM -> RAM
+run ram = V.foldl' eval ram offsets
+  where offsets = V.fromList [n | n <- [0..V.length ram], n `mod` 4 == 0]
+        eval akku offset =
+          case akku `atOffset` offset of
+            Just opcode -> perform opcode akku
+            Nothing     -> akku
+  
 main :: IO ()
 main = do
   input <- BS.getContents
   let program = parseProgram $ BS.split ',' input
-  let offsets = [offset | offset <- [0..V.length program], offset `mod` 4 == 0]
-  let opcodes = V.fromList $ catMaybes $ map (`atOffset` program) offsets
-  print $ V.foldr' perform program opcodes
+  print $ run $ program -- V.// [(1, 12), (2, 2)]
   
