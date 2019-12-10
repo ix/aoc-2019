8927fd2f5a3d6555e8daf0e38a243a46ae0d592e
Rose <rose@lain.org.uk>
2019-12-10 00:12:30 +0000

    small optimization

--- 3a.hs
+++ 3a.hs
@@ -14,18 +14,22 @@ module Main where
 import           Control.DeepSeq       (NFData)
 import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as BS
+import           Data.Function         ((&))
+import           Data.Hashable
 import           Data.HashSet          (HashSet)
 import qualified Data.HashSet          as HS
 import           Data.List             (foldl', sort)
 import           Data.Maybe            (mapMaybe)
 import           GHC.Generics          (Generic)
-import           Data.Function ((&))
 
-type Point = (Integer, Integer)
-type Wire  = [Movement]
+data Point = Point Integer Integer
+  deriving (Generic, NFData, Eq, Hashable)
+
+data Wire  = Wire [Movement]
+  deriving (Generic, NFData)
 
 data Movement = U Integer | L Integer | D Integer | R Integer
-  deriving (Generic, NFData, Show)
+  deriving (Generic, NFData)
 
 -- | Attempt to parse a Movement from a ByteString,
 -- returning the remainder of the ByteString too.
@@ -48,37 +52,37 @@ parseMovement = fmap fst . parseMovement'
 
 -- | Two-dimensional Manhattan distance.
 distance :: Point -> Point -> Integer
-distance (x, y) (x', y') = abs (x - x') + abs (y - y')
+distance (Point x y) (Point x' y') = abs (x - x') + abs (y - y')
 
 -- | Update a Point by applying a Movement to it.
 move :: Point -> Movement -> Point
-move (x, y) (U n) = (x, y + n)
-move (x, y) (D n) = (x, y - n)
-move (x, y) (R n) = (x + n, y)
-move (x, y) (L n) = (x - n, y)
+move (Point x y) (U n) = Point x $ y + n
+move (Point x y) (D n) = Point x $ y - n
+move (Point x y) (R n) = Point (x + n) y
+move (Point x y) (L n) = Point (x - n) y
 
 -- | Return a "path" (list of all passed points) for a Movement from a Point.
 path :: Point -> Movement -> HashSet Point
-path (x, y) (U n) = HS.fromList [(x', y') | x' <- [x], y' <- [y .. y + n]]
-path (x, y) (R n) = HS.fromList [(x', y') | x' <- [x .. x + n], y' <- [y]]
-path (x, y) (D n) = HS.fromList [(x', y') | x' <- [x], y' <- reverse [y - n .. y]]
-path (x, y) (L n) = HS.fromList [(x', y') | x' <- reverse [x - n .. x], y' <- [y]]
+path (Point x y) (U n) = HS.fromList [Point x' y' | x' <- [x], y' <- [y .. y + n]]
+path (Point x y) (R n) = HS.fromList [Point x' y' | x' <- [x .. x + n], y' <- [y]]
+path (Point x y) (D n) = HS.fromList [Point x' y' | x' <- [x], y' <- reverse [y - n .. y]]
+path (Point x y) (L n) = HS.fromList [Point x' y' | x' <- reverse [x - n .. x], y' <- [y]]
 
 -- | Run a Wire and return its visited locations.
 runWire :: Wire -> HashSet Point
-runWire = snd . foldl' update ((0, 0), HS.empty)
+runWire (Wire w) = snd $ foldl' update (Point 0 0, HS.empty) w
   where
     update :: (Point, HashSet Point) -> Movement -> (Point, HashSet Point)
     update (point, locs) movement =
       (move point movement, HS.union (path point movement) locs)
 
 processInput :: ByteString -> [Wire]
-processInput = map (mapMaybe parseMovement . BS.split ',') . BS.lines
+processInput = map (Wire . mapMaybe parseMovement . BS.split ',') . BS.lines
 
 main :: IO ()
 main = do
   wires <- processInput <$> BS.getContents
   let (w1, w2) = (wires !! 0, wires !! 1) in do
     print $ runWire w1 `HS.intersection` runWire w2
-      & HS.map (distance (0, 0))
+      & HS.map (distance $ Point 0 0)
       & HS.toList & filter (> 0) & sort
